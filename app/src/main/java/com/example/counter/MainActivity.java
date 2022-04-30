e9d3d4247a9c1f4720010a9734e9d3cb45defbfd
hurmainn <hurmainjavaid18@gmail.com>
2022-04-30 18:17:21 +0500

    Counter updated (stille eroneous)

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.counter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int mCounter = 0;
+    Button btn;
+    TextView txv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (android.widget.Button) findViewById (R.id.bt);
+        txv = (TextView) findViewById (R.id.count);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                {
+                    mCounter++;
+                    txv.setText(Integer.toString(mCounter));
+                }
+            }
+        });
     }
 }
